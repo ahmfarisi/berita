f37fafa44b62325ad36dd8c531913aa8ea90d40f
Ahmad Farisi <ahmfarisi@gmail.com>
2024-03-12 14:23:18 +0700

    Finish Project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:berita/models/berita.dart';
+import 'package:berita/services/api_services.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiServices _apiServices = ApiServices();
+
+  List<Berita> _berita = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHeadLines();
+    initializeDateFormatting('id_ID', null);
+  }
+
+  Future<void> _loadHeadLines() async {
+    final List<Map<String, dynamic>> allHeadLinesData =
+        await _apiServices.getHeadLines();
+    setState(() {
+      _berita = allHeadLinesData.map((e) => Berita.fromJson(e)).toList();
+    });
+  }
+
+  void _launchURL(String link) async {
+    final Uri url = Uri.parse(link);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Berita"),
+      ),
+      body: ListView.builder(
+        itemCount: _berita.length,
+        itemBuilder: (context, index) {
+          final Berita berita = _berita[index];
+          return Card(
+            margin:
+                const EdgeInsets.only(top: 8, left: 16, right: 16, bottom: 8),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  berita.title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                subtitle: Text(DateFormat('dd MMMM yyyy', 'id')
+                    .format(DateTime.parse(berita.publishedAt))),
+                onTap: () async {
+                  _launchURL(berita.url);
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
-  static const String baseUrl = "newsapi.org/v2";
+  static const String baseUrl = "https://newsapi.org/v2";
   static const String apiKey = "de35f65cabd4404ebecca7036132be1e";
 
   Future<List<Map<String, dynamic>>> getHeadLines() async {
     final response = await http
         .get(Uri.parse("$baseUrl/top-headlines?country=id&apiKey=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['results']);
+    return List<Map<String, dynamic>>.from(data['articles']);
   }
 }
